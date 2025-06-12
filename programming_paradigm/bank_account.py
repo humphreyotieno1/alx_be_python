3da10589aeb63e644a23493c0d3d8ba904805e65
humphreyotieno1 <humphreyotieno04@gmail.com>
2025-06-12 18:36:53 +0300

    Simple bank account implementation

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,20 @@
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        self.account_balance = initial_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.account_balance += amount
+        else:
+            print("Deposit amount must be positive.")
+
+    def withdraw(self, amount):
+        if amount <= self.account_balance:
+            self.account_balance -= amount
+            return True
+        else:
+            print("Insufficient funds to withdraw.")
+            return False
+
+    def display_balance(self):
+        print(f"Current balance: {self.account_balance}")
