291b3ece350aa0e0bb10e6d6c793e6bb29689e3c
humphreyotieno1 <humphreyotieno04@gmail.com>
2025-06-19 13:38:53 +0300

    Inheritance and composition

--- oop/library_system.py
+++ oop/library_system.py
@@ -22,3 +22,29 @@ class PrintBook(Book):
     
     def __str__(self):
         return f"{super().__str__()}, Page Count: {self.page_count}"
+        
+# Composition
+class Library:
+    def __init__(self):
+        self.books = []
+        
+    def add_book(self, book):
+        if isinstance(book, Book):
+            self.books.append(book)
+            return True
+        else:
+            print("Error: Only Book objects can be added to the library")
+            return False
+            
+    def list_books(self):
+        if not self.books:
+            print("The library is empty")
+            return
+        
+        for book in self.books:
+            if isinstance(book, EBook):
+                print(f"EBook: {book.title} by {book.author}, File Size: {book.file_size}KB")
+            elif isinstance(book, PrintBook):
+                print(f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}")
+            elif isinstance(book, Book):
+                print(f"Book: {book.title} by {book.author}")
