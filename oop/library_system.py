d9334882cfe7939080a18031dcfd941ef7a0468f
humphreyotieno1 <humphreyotieno04@gmail.com>
2025-06-19 13:37:47 +0300

    Inheritance and composition

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,39 +3,22 @@ class Book:
     def __init__(self, title, author):
         self.title = str(title)
         self.author = str(author)
+    
+    def __str__(self):
+        return f"{self.title} by {self.author}"
         
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = int(file_size)
+    
+    def __str__(self):
+        return f"{super().__str__()}, File Size: {self.file_size}KB"
         
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = int(page_count)
-        
-# Composition
-class Library:
-    def __init__(self):
-        self.books = []
-        
-    def add_book(self, book):
-        if isinstance(book, Book):
-            self.books.append(book)
-            return True
-        else:
-            print("Error: Only Book objects can be added to the library")
-            return False
-            
-    def list_books(self):
-        if not self.books:
-            print("The library is empty")
-            return
-        
-        for book in self.books:
-            if isinstance(book, EBook):
-                print(f"EBook: {book.title} by {book.author}, File Size: {book.file_size}KB")
-            elif isinstance(book, PrintBook):
-                print(f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}")
-            elif isinstance(book, Book):
-                print(f"Book: {book.title} by {book.author}")
+    
+    def __str__(self):
+        return f"{super().__str__()}, Page Count: {self.page_count}"
